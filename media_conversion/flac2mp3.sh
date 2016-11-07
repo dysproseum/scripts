5ff236555150e3e85f899358933d8ec8ba74d859
David Younker <david.younker@mediacurrent.com>
2016-11-06 23:29:19 -0500

    Update flac2mp3 bitrate to 320

--- media_conversion/flac2mp3.sh
+++ media_conversion/flac2mp3.sh
@@ -1,4 +1,4 @@
 for i in *.flac ; 
-do mplayer -vo null -vc dummy -af resample=44100 -ao pcm:waveheader "$i" && lame -m j -h --vbr-new -b 128 audiodump.wav -o "`basename "$i" .m4a`.mp3"; 
+do mplayer -vo null -vc dummy -af resample=44100 -ao pcm:waveheader "$i" && lame -m j -h --vbr-new -b 320 audiodump.wav -o "`basename "$i" .flac`.mp3"; 
 done; 
 rm -f audiodump.wav
