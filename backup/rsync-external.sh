0f148ef137013eef235a2325c7a11d09d07c0219
David Younker <david.younker@mediacurrent.com>
2016-11-06 23:28:39 -0500

    Update backup scripts for new IPs

--- backup/rsync-external.sh
+++ backup/rsync-external.sh
@@ -2,4 +2,4 @@
 
 # syncs data with a video symlink to data with a video dir
 
-rsync -av --delete --copy-dirlinks --exclude=video --skip-compress=* /mnt/data/ david@192.168.1.67:/mnt/1
+rsync -av --delete --copy-dirlinks --exclude=video --skip-compress=* /mnt/data/ /mnt/1
--- backup/rsync.sh
+++ backup/rsync.sh
@@ -2,4 +2,4 @@
 
 # syncs data with a video symlink to data with a video dir
 
-rsync -av --delete --copy-dirlinks --skip-compress=* /mnt/data/ david@192.168.1.67:/mnt/data
+rsync -av --delete --copy-dirlinks --skip-compress=* /mnt/data/ david@192.168.0.66:/mnt/data
