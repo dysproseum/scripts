9ae37e4f42f38c1e4d9a4a2994b4e8f511267733
David Younker <david.younker@mediacurrent.com>
2017-04-21 17:03:02 -0400

    Updated port forwarding script

--- js/reset_router_rules.js
+++ js/reset_router_rules.js
@@ -1,12 +1,27 @@
+/**
+ * Reset port forwarding rules on CenturyLink C2100T router.
+ *
+ * Rules have problems from time to time when they disappear
+ * and can't be re-added.
+ *
+ * Resetting them with this script is faster than calling tech support.
+ */
 function run(start, limit) {
-  console.log(start);
+  console.log(start + "(session " + sessionKey + ")");
   if (start >= limit) return;
 
-  var curl = "http://192.168.0.1/as_advancedportforwarding.cgi?action=remove&instance=" + start + "&sessionKey=1827193950&_=1486141918812";
-  $.get(curl, function() {
+  var curl = "http://192.168.0.1/as_advancedportforwarding.cgi?action=remove&instance=" + start + "&sessionKey=" + sessionKey + "&_=" + Math.floor(Date.now() / 1000);
+  $.get(curl, function(data) {
+      // Update new sessionKey value in response.
+      $(".port-forwarding").html(data);
       start++;
       run(start, limit)
     }
   );
 }
-run(5001, 10000);
+
+$("body").append('<div style="display:none" class="port-forwarding"></div>');
+
+// Run with this range of instance id's.
+// Add and remove a new rule in the GUI to see what its instance id was.
+run(10000,15000);
